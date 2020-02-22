50454f4e1104831c9e7756259df0a61048c0babe
Justin Lim <nockmss@gmail.com>
2020-02-22 23:22:03 +0000

    UI iteration

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace WhiskyTest.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap application_view_list {
+            get {
+                object obj = ResourceManager.GetObject("application_view_list", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap comment_add {
             get {
                 object obj = ResourceManager.GetObject("comment_add", resourceCulture);
